f9d735b85e955d982ca8287ed1ea251b73321409
Diziano <dizianomachado@gmail.com>
2020-04-21 16:10:28 -0300

    Some improvements added

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,25 +24,29 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    const income = this.transactions.reduce((total, transaction) => {
-      if (transaction.type === 'income') {
-        return total + transaction.value;
-      }
-      return total;
-    }, 0);
+    const balance = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            accumulator.total += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            accumulator.total -= transaction.value;
+            break;
+          default:
+            break;
+        }
 
-    const outcome = this.transactions.reduce((total, transaction) => {
-      if (transaction.type === 'outcome') {
-        return total + transaction.value;
-      }
-      return total;
-    }, 0);
-
-    const balance = {
-      income,
-      outcome,
-      total: income - outcome,
-    };
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
 
     return balance;
   }
